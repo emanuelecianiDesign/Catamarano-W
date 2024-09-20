b3f4a5cad2367da653f26a6b1975fbb811ea8e8a
emanuelecianiDesign <emanuele.ciani97@gmail.com>
2024-09-20 05:02:08 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,449 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cockpit",
+      "name": "Cockpit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 3.1398226439494668,
+        "pitch": 0.20771971657456945,
+        "fov": 1.3461017108479196
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9388108392039607,
+          "pitch": 0.11559758944822818,
+          "rotation": 3.141592653589793,
+          "target": "5-galley"
+        },
+        {
+          "yaw": -2.799698541313141,
+          "pitch": 0.07042090577940563,
+          "rotation": 3.141592653589793,
+          "target": "3-dinette"
+        },
+        {
+          "yaw": -2.5069697380670313,
+          "pitch": 0.047994437997207484,
+          "rotation": 3.141592653589793,
+          "target": "4-sidewalk"
+        },
+        {
+          "yaw": -1.137837614691838,
+          "pitch": 0.46614655890057044,
+          "rotation": 3.141592653589793,
+          "target": "7-opening-sides"
+        },
+        {
+          "yaw": -0.0991015221460767,
+          "pitch": 0.6826768921583977,
+          "rotation": 3.141592653589793,
+          "target": "6-sunset"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-helm-station",
+      "name": "Helm Station",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -1.9073988722182822,
+        "pitch": 0.17183631296976642,
+        "fov": 1.3461017108479196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6126584942159212,
+          "pitch": 0.22489459541638013,
+          "rotation": 3.141592653589793,
+          "target": "5-galley"
+        },
+        {
+          "yaw": -2.347035077445774,
+          "pitch": 0.11281272200211667,
+          "rotation": 3.141592653589793,
+          "target": "3-dinette"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bow-net",
+      "name": "Bow Net",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -3.123973711290743,
+        "pitch": 0.5087038400690673,
+        "fov": 1.3461017108479196
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0336047230560474,
+          "pitch": 0.19027994275012539,
+          "rotation": 3.141592653589793,
+          "target": "8-bow"
+        },
+        {
+          "yaw": 2.519897262429515,
+          "pitch": 0.11211457700257199,
+          "rotation": 1.5707963267948966,
+          "target": "4-sidewalk"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dinette",
+      "name": "Dinette",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.0404595944747363,
+        "pitch": 0.19368840786135522,
+        "fov": 1.3461017108479196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.032700233171473414,
+          "pitch": 0.2123361133311672,
+          "rotation": 3.141592653589793,
+          "target": "7-opening-sides"
+        },
+        {
+          "yaw": 1.6393751003830674,
+          "pitch": 0.16948550578726085,
+          "rotation": 3.141592653589793,
+          "target": "5-galley"
+        },
+        {
+          "yaw": 2.1847588421720054,
+          "pitch": 0.1500992921525519,
+          "rotation": 3.141592653589793,
+          "target": "1-helm-station"
+        },
+        {
+          "yaw": 0.40972524301773205,
+          "pitch": 0.19705692843086275,
+          "rotation": 3.141592653589793,
+          "target": "6-sunset"
+        },
+        {
+          "yaw": -0.8107402558288648,
+          "pitch": 0.22491791807860828,
+          "rotation": 3.141592653589793,
+          "target": "4-sidewalk"
+        },
+        {
+          "yaw": 0.5997974078670563,
+          "pitch": 0.18175731335847622,
+          "rotation": 3.141592653589793,
+          "target": "0-cockpit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sidewalk",
+      "name": "SideWalk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 3.004536982183506,
+        "pitch": 0.5319225284599973,
+        "fov": 1.3461017108479196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.021260385963487,
+          "pitch": 0.15327713675785581,
+          "rotation": 3.141592653589793,
+          "target": "8-bow"
+        },
+        {
+          "yaw": 3.0351333017660913,
+          "pitch": 0.1496124206544991,
+          "rotation": 3.141592653589793,
+          "target": "2-bow-net"
+        },
+        {
+          "yaw": 0.2963191200959798,
+          "pitch": 0.5099307570673339,
+          "rotation": 3.141592653589793,
+          "target": "7-opening-sides"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-galley",
+      "name": "Galley",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -0.9967169567842404,
+        "pitch": 0.27996329219946325,
+        "fov": 1.3461017108479196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2569194605977678,
+          "pitch": 0.21991388108565957,
+          "rotation": 3.141592653589793,
+          "target": "0-cockpit"
+        },
+        {
+          "yaw": -0.5648589028663871,
+          "pitch": 0.19518115889591314,
+          "rotation": 3.141592653589793,
+          "target": "7-opening-sides"
+        },
+        {
+          "yaw": -1.4677857667664327,
+          "pitch": 0.07916911075551347,
+          "rotation": 3.141592653589793,
+          "target": "4-sidewalk"
+        },
+        {
+          "yaw": -1.9816785315962129,
+          "pitch": 0.14976728874230005,
+          "rotation": 3.141592653589793,
+          "target": "3-dinette"
+        },
+        {
+          "yaw": 2.8085031469711623,
+          "pitch": 0.14359034824386185,
+          "rotation": 3.141592653589793,
+          "target": "1-helm-station"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sunset",
+      "name": "Sunset",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 3.126235650704638,
+        "pitch": 0.31834750423515246,
+        "fov": 1.3461017108479196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6116932156853423,
+          "pitch": 0.49726246896817017,
+          "rotation": 3.141592653589793,
+          "target": "7-opening-sides"
+        },
+        {
+          "yaw": -3.1033091211486568,
+          "pitch": 0.7306947402283477,
+          "rotation": 3.141592653589793,
+          "target": "0-cockpit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-opening-sides",
+      "name": "Opening Sides",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -2.996732900118694,
+        "pitch": 0.36594295876465566,
+        "fov": 1.3461017108479196
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.299508438757531,
+          "pitch": 0.8267687122559977,
+          "rotation": 3.141592653589793,
+          "target": "6-sunset"
+        },
+        {
+          "yaw": 3.1092102605737697,
+          "pitch": 0.10704923171266856,
+          "rotation": 3.141592653589793,
+          "target": "5-galley"
+        },
+        {
+          "yaw": -2.5233504097187307,
+          "pitch": 0.06063898989499705,
+          "rotation": 3.141592653589793,
+          "target": "4-sidewalk"
+        },
+        {
+          "yaw": -2.7292974704761743,
+          "pitch": 0.09900556340202016,
+          "rotation": 3.141592653589793,
+          "target": "3-dinette"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bow",
+      "name": "Bow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -3.0171496694196662,
+        "pitch": 0.42423405000645076,
+        "fov": 1.3461017108479196
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7677557006834643,
+          "pitch": 0.7450099070266951,
+          "rotation": 3.141592653589793,
+          "target": "2-bow-net"
+        },
+        {
+          "yaw": 2.55865682910429,
+          "pitch": 0.1486192999481446,
+          "rotation": 3.141592653589793,
+          "target": "4-sidewalk"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
